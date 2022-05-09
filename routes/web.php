15c1727fb44651e851f8e99c6760f92a85aac5bf
syahrizal-alisadikin <alisadikinsyahrizal@gmail.com>
2022-05-09 17:09:55 +0700

    crud category + products

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,14 @@ Route::prefix('apps')->group(function () {
         //route resource users
         Route::resource('/users', UserController::class, ['as' => 'apps'])
             ->middleware('permission:users.index|users.create|users.edit|users.delete');
+
+        //route resource categories
+        Route::resource('/categories', \App\Http\Controllers\Apps\CategoryController::class, ['as' => 'apps'])
+            ->middleware('permission:categories.index|categories.create|categories.edit|categories.delete');
+
+        //route resource products
+        Route::resource('/products', \App\Http\Controllers\Apps\ProductController::class, ['as' => 'apps'])
+            ->middleware('permission:products.index|products.create|products.edit|products.delete');
     });
 });
 Route::get('auth/google', [GoogleController::class, 'redirectToGoogle']);
